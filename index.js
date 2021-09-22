3cd69e4b6c9191062aa0813803978a211523d908
Todd Krieger <twkriege@mtu.edu>
2021-09-22 18:09:17 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+// //Function identifies spaces in a sentance, the character after a space must be a new word,
+// //the first letter of which needs to be capitalized. 
+// function findWords (string) {
+//   let spaces = [];
+//   spaces[0] = 0;
+//   j = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string.charAt(i) === ' ') {
+//       spaces[j] = i;
+//       j++;
+//       //console.log(i);
+//     };
+//   };
+// //console.log(spaces);
+// return spaces;
+// };
+
+// breakString = (sentance) => {
+//   let sentanceArray = sentance.split(' ');
+//   return sentanceArray;
+//   console.log(sentanceArray);
+// }
+
+function tCaseString (value, index, arr) {
+  let splitArray = value.split(' ');
+  let newArray = [];
+  for (let i = 0; i < splitArray.length; i++) {
+      let temp = splitArray[i][0].toUpperCase() + splitArray[i].slice(1);
+      newArray.push(temp);
+      //console.log(newArray);
+  }
+  return newArray.join(' ');
+  
 }
+
+
+const titleCased = () => {
+  
+  let improved = tutorials.map(tCaseString);
+
+
+  return improved;
+
+}
+//titleCased(tutorials);
